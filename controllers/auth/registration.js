e002f30a58c31b4ac3d022a1d54372c85ecd9fb0
NEYROSICK <dhatak24@gmail.com>
2023-11-02 20:59:21 +0200

    Added user gravatar
    Added user avatar using gravatar library to create and store default avatar path after registration

--- controllers/auth/registration.js
+++ controllers/auth/registration.js
@@ -1,8 +1,11 @@
 const User = require("../../models/user");
 const bcrypt = require("bcrypt");
+const gravatar = require("gravatar");
 
 const register = async (req, res, next) => {
   req.body.password = await bcrypt.hash(req.body.password, 10);
+  req.body.avatarURL = gravatar.url(req.body.email);
+
   const user = await User.create(req.body);
   const response = {
     user: { email: user.email, subscription: user.subscription },
