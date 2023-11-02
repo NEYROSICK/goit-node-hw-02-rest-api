56fabe966f3b06e82717f42042697027316728c0
NEYROSICK <dhatak24@gmail.com>
2023-11-02 23:25:27 +0200

    Upload image option
    1. Added upload middleware
    2. Added image moving and naming logic

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,8 +1,10 @@
 const getCurrentUser = require("./getCurrentUser");
 const updateSubscription = require("./updateSubscription");
+const updateAvatar = require("./updateAvatar");
 const wrapper = require("../../helpers/controllerWrapper");
 
 module.exports = {
   getCurrentUser: wrapper(getCurrentUser),
   updateSubscription: wrapper(updateSubscription),
+  updateAvatar: wrapper(updateAvatar),
 };
