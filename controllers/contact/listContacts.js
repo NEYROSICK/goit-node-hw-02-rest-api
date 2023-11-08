f783ebd2de11aa5f987eb5a667901e73b3451527
NEYROSICK <dhatak24@gmail.com>
2023-11-08 12:07:10 +0200

    Added pagination
    Added request parameters for pagination (skip & page)

--- controllers/contact/listContacts.js
+++ controllers/contact/listContacts.js
@@ -1,7 +1,18 @@
 const Contact = require("../../models/contact");
+const requestError = require("../../helpers/requestError");
 
 const listContacts = async (req, res, next) => {
-  const result = await Contact.find();
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 0;
+  const skip = Number(page) <= 1 ? 0 : (page - 1) * limit;
+  const contactCount = await Contact.countDocuments();
+
+  if (skip >= contactCount) {
+    throw requestError(400, "Out of range request");
+  }
+
+  console.log(skip);
+  const result = await Contact.find({}, null, { skip }).limit(limit);
   return res.status(200).json(result);
 };
 
