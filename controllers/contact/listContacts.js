31dd65dd5cea9bd0deb1ae7976947a2ea7847578
NEYROSICK <dhatak24@gmail.com>
2023-11-08 12:23:41 +0200

    Added favorite filter
    Added search params favorite filter

--- controllers/contact/listContacts.js
+++ controllers/contact/listContacts.js
@@ -2,17 +2,19 @@ const Contact = require("../../models/contact");
 const requestError = require("../../helpers/requestError");
 
 const listContacts = async (req, res, next) => {
-  const page = req.query.page || 1;
-  const limit = req.query.limit || 0;
+  const { page = 1, limit = 0, favorite } = req.query;
   const skip = Number(page) <= 1 ? 0 : (page - 1) * limit;
   const contactCount = await Contact.countDocuments();
-
   if (skip >= contactCount) {
     throw requestError(400, "Out of range request");
   }
 
-  console.log(skip);
-  const result = await Contact.find({}, null, { skip }).limit(limit);
+  const query = {};
+  if (favorite) {
+    query.favorite = favorite;
+  }
+
+  const result = await Contact.find(query, null, { skip }).limit(limit);
   return res.status(200).json(result);
 };
 
